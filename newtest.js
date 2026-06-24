aa5e4ab736811c4270e3717b6efc3943c1331357
sanbukkarasi29 <sanbukkarasi29@gmail.com>
2026-06-25 00:29:35 +0530

    file

---
+++ newtest.js
@@ -0,0 +1,7 @@
+import{test} from '@playwright/test';
+test("validate facebook Application", async( {page} ) => {
+    await page.goto("https://www.facebook.com/login/?next=https%3A%2F%2Fwww.facebook.com%2F");
+    console.log(" test file moved to github")
+    console.log("welcome new branch")
+    console.log("playwright1")
+} )
